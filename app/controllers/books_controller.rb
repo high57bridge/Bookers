a9854572dc38cee0193d61a83096a4789cb1ab20
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-01 11:45:56 +0000

    [next エラー解消index]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,37 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books/index'
+  end
+  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.index(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created." 
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "Book was posting failed."
+      render :index
+    end
   end
 
   def index
+    @book = Book.index
     @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
   end
+  
+    def back
+      book = Book.new(book_params)
+      book.back
+      redirect_to '/books/index'
+    end
 
   def edit
     @book = Book.find(params[:id])
@@ -25,12 +43,6 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
-  end
-  
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
   end
   
   private
