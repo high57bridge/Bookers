816bab531fa4b3c168cff248e15ca82a5e5d9b5f
EC2 Default User <149574851+high57bridge@users.noreply.github.com>
2023-12-03 15:07:22 +0000

    [editした後の遷移先,ホームのレイアウト,testして赤い部分の修正]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @book = Book.new(book_params)
   end
-  
+
   def create
     @books = Book.all
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully created." 
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       flash.now[:alert] = "Book was posting failed."
@@ -16,24 +17,22 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.new
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @books = Book.all
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
     def back
-      book = Book.find(params[:id])
-      book.back
       redirect_to '/books'
     end
 
@@ -41,24 +40,25 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     @book = Book.new(book_params)
     if @book.save
-      flash[:notice] = "Book was successfully updated." 
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       flash.now[:alert] = "Book was updating failed."
+       @books = Book.all
       render :index
     end
   end
-  
+
   def start
     redirect_to '/books'
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   get 'books/new' => 'books#new', as: 'new_book'
   get '/top' => 'homes#top'
   post 'books' => 'books#create'
-  get 'books' => 'books#index' 
+  get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit',as: 'edit_book'
-  patch 'books/:id' => 'books#update',as: 'update_book'
+  patch 'books/:id/edit' => 'books#update',as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  get 'books/:id' => 'books#back', as: 'back_book'
+  get 'books' => 'books#back', as: 'back_book'
   get 'books/new' => 'books#start', as: 'start_book'
   resources :books
   # For details on the DSL available  within this file, see https://guides.rubyonrails.org/routing.html
